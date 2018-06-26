d5cd6b9f39e43008a492afde876ffc2e6e31acb5
hejack0207 <hejack0207@sina.com>
2018-06-26 09:37:44 +0800

    add .gitattributes

--- lib/run.ts
+++ lib/run.ts
@@ -31,11 +31,11 @@ const run = async () => {
   await page.click("#zisubmit");
   await page.waitForNavigation({ waitUntil: 'networkidle2' });
 
-  const brief_output = await page.$eval('.text15',e => e.innerText);
-  const folk_output = await page.$eval('#div_a1 > div:nth-child(7)',e => e.innerText);
-  const glyph_output = await page.$eval('#div_a1 > div:nth-child(8)',e => e.innerText)
-  const expla_output = await page.$eval('#div_a1 > div:nth-child(9)',e => e.innerText)
-  const phonation_output = await page.$eval('#div_a1 > div:nth-child(10)',e => e.innerText)
+  const brief_output = await page.$eval('.text15',e => e.outerHTML);
+  const folk_output = await page.$eval('#div_a1 > div:nth-child(7)',e => e.outerHTML);
+  const glyph_output = await page.$eval('#div_a1 > div:nth-child(8)',e => e.outerHTML)
+  const expla_output = await page.$eval('#div_a1 > div:nth-child(9)',e => e.outerHTML)
+  const phonation_output = await page.$eval('#div_a1 > div:nth-child(10)',e => e.outerHTML)
   var definition = { brief : brief_output, folk : folk_output, glyph : glyph_output, expla : expla_output, phonation : phonation_output };
   //console.log('definition:%j',definition);
   p.brief && console.log(brief_output+"\n");
