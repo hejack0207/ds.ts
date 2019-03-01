b0421d0db90ef5f2e16490ad4eebe70cb516d2cd
hejack0207 <hejack0207@sina.com>
2019-03-01 17:10:54 +0800

    fix

--- lib/sg.ts
+++ lib/sg.ts
@@ -14,16 +14,23 @@ export const run = async (browser: puppeteer.Browser) => {
   try{
     await page.goto(url);
     //await new Promise(x => setTimeout(x, 3000));
+
+    await page.waitForSelector("#loginid");
+    await page.type("#loginid", "何龙");
+    await page.type("#userpassword", "ly123qwe");
+    await page.click("#login");
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
     await page.screenshot({path: "/tmp/sg.png"});
+
+    /*
+    await page.click("div[title='流程']");
+    await page.click("#subform > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr > td > table > tbody > tr > td:nth-child(5) > div:nth-child(1) > ul > li:nth-child(4) > a");
+    */
+    await page.goto("http://172.16.0.251:81/workflow/request/AddRequest.jsp?workflowid=186&isagent=0&beagenter=0");
+
+    await browser.close();
   }catch(e){
     console.log("error:"+e);
   }
-
-//  await page.waitForSelector("#wd");
-//  await page.type("#wd", hanzi);
-//  await page.click("#zisubmit");
-//  await page.waitForNavigation({ waitUntil: 'networkidle2' });
-
-  await browser.close();
   return "abc";
 };
