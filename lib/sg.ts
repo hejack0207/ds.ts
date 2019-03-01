ace22ec82fa53b57bfae7804e92277e7aa1e1aa5
hejack0207 <hejack0207@sina.com>
2019-03-01 17:19:51 +0800

    fix

--- lib/sg.ts
+++ lib/sg.ts
@@ -20,14 +20,15 @@ export const run = async (browser: puppeteer.Browser) => {
     await page.type("#userpassword", "ly123qwe");
     await page.click("#login");
     await page.waitForNavigation({ waitUntil: 'networkidle2' });
-    await page.screenshot({path: "/tmp/sg.png"});
 
     /*
     await page.click("div[title='流程']");
     await page.click("#subform > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr > td > table > tbody > tr > td:nth-child(5) > div:nth-child(1) > ul > li:nth-child(4) > a");
     */
     await page.goto("http://172.16.0.251:81/workflow/request/AddRequest.jsp?workflowid=186&isagent=0&beagenter=0");
+    await page.waitForSelector("#\\24addbutton0\\24");
 
+    await page.screenshot({path: "/tmp/sg.png"});
     await browser.close();
   }catch(e){
     console.log("error:"+e);
